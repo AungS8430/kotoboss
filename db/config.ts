43cae3d60686e93d3f03e686800dc0c397c2eb8b
AungS8430 <aungshome@outlook.com>
2025-06-21 20:22:11 +0700

    Configured a basic database for storing deck and cards, also added deck display page

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,33 @@
-import { defineDb } from 'astro:db';
+import { defineDb, defineTable, column } from 'astro:db';
+
+const Card = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    difficulty: column.number(),
+    due: column.date(),
+    elapsed_days: column.number(),
+    lapses: column.number(),
+    last_review: column.date({ optional: true }),
+    learning_steps: column.number(),
+    reps: column.number(),
+    scheduled_days: column.number(),
+    stability: column.number(),
+    state: column.number(),
+    media: column.text()
+  }
+})
+
+const Deck = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    user: column.text(),
+    name: column.text(),
+    max_new_cards: column.number(),
+    max_reviews: column.number(),
+  }
+})
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: { Card, Deck }
 });
--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,10 @@
-import { db } from 'astro:db';
+import { db, Deck, Card } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+	await db.insert(Deck).values([
+		{ id: 1, user: "aungshome@outlook.com", name: "Test 1", max_new_cards: 10, max_reviews: 100 },
+		{ id: 2, user: "aungshome@outlook.com", name: "Test 2", max_new_cards: 10, max_reviews: 100 },
+		{ id: 3, user: "john@doe.gg", name: "Not Shown", max_new_cards: 10, max_reviews: 100}
+	])
 }
