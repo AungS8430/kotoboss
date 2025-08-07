7ef317e10fee72a6035a61b6ea8e3d913dc2670c
AungS8430 <aungshome@outlook.com>
2025-08-07 19:18:48 +0700

    Fixed Google Auth session

--- auth.config.ts
+++ auth.config.ts
@@ -1,24 +1,28 @@
 import Google from "@auth/core/providers/google";
 import { defineConfig } from "auth-astro";
+import Credentials from "@auth/core/providers/credentials";
 
 export default defineConfig({
   providers: [
     Google({
       clientId: import.meta.env.AUTH_GOOGLE_ID,
-      clientSecret: import.meta.env.AUTH_GOOGLE_SECRET
-    })
+      clientSecret: import.meta.env.AUTH_GOOGLE_SECRET,
+      async profile(profile) {
+        return { ...profile }
+      },
+    }),
+    Credentials({
+      async authorize(credentials) {
+        return { ...credentials }
+      },
+    }),
   ],
+
   callbacks: {
     jwt({ token, user }) {
-      if (user) {
-        token.id = user.id;
-      }
       return token;
     },
     session({ session, token }) {
-      if (token.email) {
-        session.user.id = token.id;
-      }
       return session;
     }
   }
